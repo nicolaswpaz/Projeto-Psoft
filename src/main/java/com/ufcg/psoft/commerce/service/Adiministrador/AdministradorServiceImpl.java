8ecbe32f78599c38b59e983eeec79617c4d50d67
CarlosArturr <carlos.artur.santana.sales@ccc.ufcg.edu.br>
2025-07-16 14:08:46 -0300

    teste

--- src/main/java/com/ufcg/psoft/commerce/service/Adiministrador/AdministradorServiceImpl.java
+++ src/main/java/com/ufcg/psoft/commerce/service/Adiministrador/AdministradorServiceImpl.java
@@ -1,14 +1,15 @@
 package com.ufcg.psoft.commerce.service.Adiministrador;
 
 
+import com.ufcg.psoft.commerce.dto.Cliente.ClientePostPutRequestDTO;
+import com.ufcg.psoft.commerce.dto.Cliente.ClienteResponseDTO;
+import com.ufcg.psoft.commerce.model.Adiministrador;
+import com.ufcg.psoft.commerce.model.Cliente;
 import com.ufcg.psoft.commerce.service.cliente.ClienteService;
 import org.springframework.stereotype.Service;
+import
 
 @Service
 public class AdministradorServiceImpl implements AdiministradorService {
 
-    @Override
-    public AdiministradorResponseDTO criar(AdiministradorPostPutRequestDTO adiministradorPostPutRequestDTO) {
-        return null;
-    }
 }
