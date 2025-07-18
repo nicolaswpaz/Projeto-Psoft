86f81ca34395346babbe2a9247db9eb641da4b17
nicolemaracaja <nicolebrito.maracaja@gmail.com>
2025-07-18 19:56:09 -0300

    Colocando anotacao de controlador em AdministradorController

--- src/main/java/com/ufcg/psoft/commerce/controller/AdministradorController.java
+++ src/main/java/com/ufcg/psoft/commerce/controller/AdministradorController.java
@@ -8,9 +8,10 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+@RestController
 public class AdministradorController {
     @Autowired
-    private AdministradorService administradorService;
+    AdministradorService administradorService;
 
     @PostMapping
     public ResponseEntity<AdministradorResponseDTO> criarAdministrador(@RequestBody AdministradorPostPutRequestDTO dto) {
