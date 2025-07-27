343c4521536ca4be2decedd3908ed471f8e5bfd3
DeboraSabrinaOPereira <debora.sabrina.oliveira.pereira@ccc.ufcg.edu.br>
2025-07-27 00:09:47 -0300

    buscar nome em cliente repository modificado

--- src/main/java/com/ufcg/psoft/commerce/repository/ClienteRepository.java
+++ src/main/java/com/ufcg/psoft/commerce/repository/ClienteRepository.java
@@ -2,11 +2,15 @@ package com.ufcg.psoft.commerce.repository;
 
 import com.ufcg.psoft.commerce.model.Cliente;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
 import java.util.List;
 
 public interface ClienteRepository extends JpaRepository<Cliente, Long> {
 
-    List<Cliente> findByNomeContaining(String nome);
+    @Query("SELECT c FROM Cliente c WHERE c.nome LIKE %:nome%")
+    List<Cliente> buscarPorNome(@Param("nome") String nome);
 
     Long id(Long id);
 }
